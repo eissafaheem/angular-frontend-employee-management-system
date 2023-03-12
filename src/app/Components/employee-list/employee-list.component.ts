2bed56d6c8d5309423152c4ba4ad36215c791855
Eissa Faheem <eissafaheem@gmail.com>
2023-03-12 13:51:22 +0530

    addde ui

--- src/app/Components/employee-list/employee-list.component.ts
+++ src/app/Components/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/Models/Classes/Employee';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+  employees:number[] = [1,2,5,8,3,1,1,1,1]
   constructor() { }
 
   ngOnInit(): void {
